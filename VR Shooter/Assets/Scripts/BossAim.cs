f612b400ddbd9e8d74b1bb40b5d8c17e6e439214
Your Name <you@example.com>
2020-12-20 12:48:51 +0700

    fixed bos prefab

--- VR Shooter/Assets/Scripts/BossAim.cs
+++ VR Shooter/Assets/Scripts/BossAim.cs
@@ -3,9 +3,18 @@ using UnityEngine.AI;
 
 public class BossAim : MonoBehaviour
 {
+    // public NavMeshAgent agent;
+
     public Transform player;
 
-    public LayerMask whatIsPlayer;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float health;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
 
     //Attacking
     public float timeBetweenAttacks;
@@ -19,6 +28,7 @@ public class BossAim : MonoBehaviour
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
+        //   agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
@@ -26,11 +36,48 @@ public class BossAim : MonoBehaviour
         //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        // if (!playerInSightRange && !playerInAttackRange) Patroling();
+        //if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        //    if (!walkPointSet) SearchWalkPoint();
+
+        //    if (walkPointSet)
+        //        agent.SetDestination(walkPoint);
+
+        //    Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //    //Walkpoint reached
+        //    if (distanceToWalkPoint.magnitude < 1f)
+        //        walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        ////Calculate random point in range
+        //float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        //float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        //walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        //if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        //    walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        //agent.SetDestination(player.position);
     }
 
     private void AttackPlayer()
     {
+        // agent.SetDestination(transform.position);
+
+        // transform.LookAt(player);
+
         if (!alreadyAttacked)
         {
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
@@ -44,5 +91,16 @@ public class BossAim : MonoBehaviour
     {
         alreadyAttacked = false;
     }
+
+    //public void TakeDamage(int damage)
+    //{
+    //    health -= damage;
+
+    //    if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    //}
+    //private void DestroyEnemy()
+    //{
+    //    Destroy(gameObject);
+    //}
 
 }
