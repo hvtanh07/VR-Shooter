e6dd24922efdca7cc736567b6d1278ae365b5d42
Your Name <you@example.com>
2020-12-20 10:43:54 +0700

    kkk

---
+++ VR Shooter/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    //Assignables
+    public Rigidbody rb;
+    public GameObject explosion;
+    GameObject Player;
+    Player player;
+    public LayerMask whatIsEnemies;
+
+    //Stats
+    [Range(0f,1f)]
+    public float bounciness;
+    public bool useGravity;
+
+    //Damage
+    public int explosionDamage;
+    public float explosionRange;
+
+    //Lifetime
+    public int maxCollisions;
+    public float maxLifetime;
+
+    private int collisions;
+    private PhysicMaterial physics_mat;
+
+    private void Start()
+    {
+        Setup();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        player = Player.GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        if (collisions >= maxCollisions) Explode();
+
+        maxLifetime -= Time.deltaTime;
+        if (maxLifetime <= 0) Explode();
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Don't count collisions with oter bullets
+        if (collision.collider.CompareTag("Bullet")) return;
+
+        //Count up collisions
+        collisions++;
+
+        //Explode if bullet hits an enemy directly and explodeOnTouch is activated
+        if (collision.collider.CompareTag("Player") )
+        {
+            Explode(); player.TakeDamage(explosionDamage);
+        }
+    }
+
+    private void Setup()
+    {
+        //Make a new Physics material
+        physics_mat = new PhysicMaterial();
+        physics_mat.bounciness = bounciness;
+        physics_mat.frictionCombine = PhysicMaterialCombine.Minimum;
+        physics_mat.bounceCombine = PhysicMaterialCombine.Maximum;
+        //Assign material to collider
+        GetComponent<SphereCollider>().material = physics_mat;
+
+        //Set gravity
+        rb.useGravity = useGravity;
+    }
+
+    /// Just to visualize the explosion range
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRange);
+    }
+}
